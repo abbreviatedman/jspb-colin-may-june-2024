51554f2a30a06399bc1a9402540453717d279a3c
Colin Jaffe <colin.jaffe@gmail.com>
2024-05-30 15:02:42 -0400

    Updates unit.

--- 05-LOOPS/05.03-While-and-Nested-Loops/js/05.03-While-and-Nested-Loops-FINAL.js
+++ 05-LOOPS/05.03-While-and-Nested-Loops/js/05.03-While-and-Nested-Loops-FINAL.js
@@ -14,49 +14,22 @@ while (n <= 10) {
 }
 console.log(n);
 
-// do while loop
+// 2. Write this while loop so that it concatenates the counter, resulting in output of 1, 12, 123, 1234, etc:
 
-// In a "do while loop", the condition is evaluated only *after* the loop has already run, and so it must run at least once.
-
-// 2. Write this 'do while' loop:
-
-let num = 0;
-do {
-  console.log(num);
-  num++;
-} while (num <= 10);
-
-/*
-Overall, while and do while loops are pretty similar:
-- both declare the counter *outside (before)* the loop
-- both increment the counter *inside* the loop
-*/
-
-// 3. Write these while and do while loops that concatenate the counter, resulting in output of 1, 12, 123, 1234, etc:
-
-let i = 0,
-  str = "";
+let i = 0
+let str = "";
 
 while (i <= 10) {
-  str += i;
-  i++;
-  console.log(str);
+    str += i;
+    i++;
+    console.log(str);
 }
-
-str = "";
-i = 10;
-
-do {
-  str += i;
-  i--;
-  console.log(str);
-} while (i >= 1);
 
 // break
 
-// Recall that **continue** skips an iteration of a loop. The keyword **break** exits the loop.
+// The keyword **break** exits the loop.
 
-// 4. Write a while loop that ends the loop when the counter gets to 7:
+// 3. Write a while loop that ends the loop when the counter gets to 7:
 
 let x = 0;
 while (x <= 10) {
@@ -68,7 +41,7 @@ while (x <= 10) {
   x++;
 }
 /*
-5. Write a while loop that iterates an array, looking for a number divisible by 7:
+4. Write a while loop that iterates an array, looking for a number divisible by 7:
 - *if* the number is found, save it to `target` and `break`. 
 - *if* no number divisible by 7 is found, set the index to -1.
 - when it's all over, log the `target` and its index. 
@@ -90,13 +63,13 @@ console.log("target", target, "index", indx);
 
 // Recall that 0 is falsey, meaning that it returns false in an if-statement. So, just to be fancy, we're using `if(!target)` rather than `if(target == 0)` -- and putting it all on one line with no curly braces.
 
-// 6. Change the array values so that the loop runs with and without finding a number divisible by 7.
+// 5. Change the array values so that the loop runs with and without finding a number divisible by 7.
 
 // nested loops:
 
 // A **nested loop** is a loop-inside-a-loop. Each time the outer loop runs once, it iterates over the entire inner loop. So, if each loop is running ten times, the total is 10x10 = 100 iterations.
 
-// 7. Run a nested loop where we output the values of both inner and outer counter as we go:
+// 6. Run a nested loop where we output the values of both inner and outer counter as we go:
 
 for (let i = 1; i <= 10; i++) {
   for (let j = 1; j <= 10; j++) {
@@ -107,13 +80,13 @@ for (let i = 1; i <= 10; i++) {
 // This next one involves an array of city abbreviations. Once again, we will use a nested loop. Both loops will iterate over the same array.
 // The inner loop will use the outer and inner counters, **i** and **j**, respectively, to concatenate a pair of cities. The names will be hyphenated, like departures-arrivals: NY-MIA
 
-// 8. Declare an array of city abbreviations:
+// 7. Declare an array of city abbreviations:
 
 const cities = ["CHI", "NY", "PHI", "BOS", "MIA", "LA", "SF"];
 
 // We will iterate the array with a for loop and console log all possible pairings of cities. We need a nested loop for this, since each city needs to be paired with every other city.
 
-// 9. Pair each city with every other city--including itself.
+// 8. Pair each city with every other city--including itself.
 
 for (let i = 0; i < cities.length; i++) {
   for (let j = 0; j < cities.length; j++) {
@@ -121,7 +94,7 @@ for (let i = 0; i < cities.length; i++) {
   }
 }
 
-// 10. Use conditional logic to only pair cities when `i` and `j` are not equal:
+// 9. Use conditional logic to only pair cities when `i` and `j` are not equal:
 
 for (let i = 0; i < cities.length - 1; i++) {
   for (let j = 0; j < cities.length; j++) {
@@ -136,7 +109,7 @@ for (let i = 0; i < cities.length - 1; i++) {
 
 // Declare an array of fruits. The challenge is to "make smoothies" as pairs of fruits. We want all possible combinations, but without any repeats in reverse, so "banana-peach" is good, but "peach-banana" is bad.
 
-// 11. Declare an array of fruits:
+// 10. Declare an array of fruits:
 
 const fruits = [
   "apple",
@@ -155,7 +128,7 @@ const fruits = [
 
 // Start with a buggy version, one that gives you all 144 possible pairings--the 12x12 max. This includes pairings of fruits with itself ("apple-apple") and revserve pairs ("apple-apricot" AND "apricot-apple"). Include a counter that we increment with each pair, so that we can know if the total is going down as we proceed:
 
-// 12. Make all 144 possible 2-fruit combinations, and keep count:
+// 11. Make all 144 possible 2-fruit combinations, and keep count:
 
 let counter = 1;
 
@@ -166,7 +139,7 @@ for (let i = 0; i < fruits.length; i++) {
   }
 }
 
-// 13. Reset the counter, and run a new loop, where you only get 132 pairs, because the self-pairings ("apple-apple") have been eliminated. This is done with conditional logic: we only make the pair if `i` and `j` are *not* equal:
+// 12. Reset the counter, and run a new loop, where you only get 132 pairs, because the self-pairings ("apple-apple") have been eliminated. This is done with conditional logic: we only make the pair if `i` and `j` are *not* equal:
 
 counter = 1;
 
@@ -183,7 +156,7 @@ for (let i = 0; i < fruits.length; i++) {
 // Upgrade again, this time to the final version. Each time through the loop, set the just-finished item to "done". Then add another condition to the if-statement requiring that the current fruit as `fruits[j]` has not yet been 'done'. The result is pairs that do not repeat in reverse. We get "apple-banana", but NOT "banana-apple".
 // Also save the fruit pairs to a smoothies array rather than just logging them to the console.
 
-// 14. Write the final version of the "smoothies maker", in accordance with the above guidelines:
+// 13. Write the final version of the "smoothies maker", in accordance with the above guidelines:
 
 counter = 1;
 const smoothies = [];
@@ -200,7 +173,7 @@ for (let i = 0; i < fruits.length; i++) {
 }
 console.log("2-fruit smoothies", smoothies);
 
-// 15. Make it into a function that takes the array as its argument, runs the "smoothie maker" code and, in good input-output fashion, returns the smoothies:
+// 14. Make it into a function that takes the array as its argument, runs the "smoothie maker" code and, in good input-output fashion, returns the smoothies:
 
 function makeSmoothies(arr) {
   counter = 1;
@@ -220,10 +193,10 @@ function makeSmoothies(arr) {
   return smoothiesArr;
 }
 
-// 16. Log fruits to see that all its items have been replaced with 'done'
+// 15. Log fruits to see that all its items have been replaced with 'done'
 console.log("fruits", fruits, fruits.length);
 
-// 17. Redeclare fruits under a new name:
+// 16. Redeclare fruits under a new name:
 
 const froots = [
   "apple",
@@ -240,7 +213,7 @@ const froots = [
   "raspberry",
 ];
 
-// 18. Declare an array of tropical fruit, so that we can test the function on more than just one array:
+// 17. Declare an array of tropical fruit, so that we can test the function on more than just one array:
 
 const tropicalFruits = [
   "banana",
@@ -257,7 +230,7 @@ const tropicalFruits = [
   "guava",
 ];
 
-// 19. Call the function twice, passing in a different array each time. Save the function call to a variable so that you capture the return value (the smoothies):
+// 18. Call the function twice, passing in a different array each time. Save the function call to a variable so that you capture the return value (the smoothies):
 
 let smoothies1 = makeSmoothies(froots);
 console.log("froot smoothies", smoothies1);
